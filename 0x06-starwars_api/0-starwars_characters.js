91be1dee9e0f2be8cc4ddaadeaf8b73bae560c7d
Nathanage3 <sisay954@gmail.com>
2024-05-24 00:41:59 +0300

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,16 @@
 
 const request = require('request');
 
-function getCharacterName(url) {
-  request(url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const json = JSON.parse(body);
-      console.log(json.name);
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
-
-function getCharactersInMovie(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  request(url, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const json = JSON.parse(body);
-      for (let character of json.characters) {
-        getCharacterName(character);
-      }
-    }
-  });
-}
-
-const movieId = process.argv[2];
-getCharactersInMovie(movieId);
+};
