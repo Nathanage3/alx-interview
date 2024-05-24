be38fb3d612ccc136b122c2e228c9b983e8a8e89
Nathanage3 <sisay954@gmail.com>
2024-05-24 14:41:33 +0300

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,11 +7,25 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
+/**
+ * exactOrder: recursive function that prints the name of each character
+ * from the array of actors in the order they appear in the array
+ * @param {array} actors - an array of character urls
+ * @param {number} x - the current index in the array
+ */
 const exactOrder = (actors, x) => {
+  // base case: if we've reached the end of the array, return
   if (x === actors.length) return;
+
+  // make a request to get the character at the current index
   request(actors[x], function (err, res, body) {
+    // handle errors
     if (err) throw err;
+    
+    // print the name of the character
     console.log(JSON.parse(body).name);
+
+    // recursively call the function with the next index
     exactOrder(actors, x + 1);
   });
 };
